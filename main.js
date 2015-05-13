85dcd7b49fdf0ab641b259cb9b4a5816ca47ab23
Robert Fine <robertfine4631@icloud.com>
2015-05-13 10:45:04 -0400

    applied changes to my original code to make it functional

--- main.js
+++ main.js
@@ -1,37 +1,20 @@
-// var $top = document.querySelector("#num_one");
-// var $bottom = document.querySelector("#num_two");
-// var $addi = document.querySelector("#calculateBtn");
-// var $ansBox = document.querySelector("#answer");
-// var answer;
-// var number1, number2;
-
-
-
-// function Addition (x,y) {
-// 	return Number(x) + Number(y);
-// }
-
-// function showValue () {
-// 	answer = Addition($top.value, $bottom.value);
-// 	$ansBox.innerHTML = answer;
-// }
-
-// addi.addEventListener('click', Addition);
-
-
-var $input1 = document.querySelector("#num_one");
-var $input2 = document.querySelector("#num_two");
-var $calculateBtn = document.querySelector("#calculateBtn");
-var $answerField = document.querySelector("#answer");
-var number1, number2;
+var $top = document.querySelector("#num_one");
+var $bottom = document.querySelector("#num_two");
+var $addi = document.querySelector("#calculateBtn");
+var $ansBox = document.querySelector("#answer");
 var answer;
-function calculate(x, y) {
-  return Number(x) + Number(y);
+var number1, number2;
+
+
+
+function Addition (x,y) {
+	return Number(x) + Number(y);
 }
 
-function showAnswer () {
-  answer = calculate($input1.value, $input2.value);
-  $answerField.innerHTML = answer;
+function showValue () {
+	answer = Addition($top.value, $bottom.value);
+	$ansBox.innerHTML = answer;
 }
 
-$calculateBtn.addEventListener('click', showAnswer);
+$addi.addEventListener('click', showValue);
+
